14951aa428880d3a4dbe41269feb6f08b15503bc
diazka_ <diazananda02@gmail.com>
2024-07-06 16:28:47 +0800

    add firebase config

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBgKz-57TToh8DqCl_4OqvrQiGC8vcNPqc",
+  authDomain: "lomboktravelia-93d61.firebaseapp.com",
+  projectId: "lomboktravelia-93d61",
+  storageBucket: "lomboktravelia-93d61.appspot.com",
+  messagingSenderId: "378263941578",
+  appId: "1:378263941578:web:3c842325ee3edcb9fbdf33",
+  measurementId: "G-B12P7KBCGC"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
