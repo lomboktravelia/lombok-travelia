fa77683c3b2206e583aa835a3a52707b5c964a2b
syahrulamri11 <syahrul11aemry@gmail.com>
2024-07-30 14:33:16 +0800

    add login with google

--- src/app/api/logout/route.js
+++ src/app/api/logout/route.js
@@ -1,6 +1,17 @@
+// import { cookies } from "next/headers";
+
+// export async function POST() {
+//   cookies().set("session", "", { expires: new Date(0) });
+//   return Response.json({ status: "success" });
+// }
+
+
 import { cookies } from "next/headers";
 
 export async function POST() {
+  // Hapus cookie sesi untuk login biasa
   cookies().set("session", "", { expires: new Date(0) });
-  return Response.json({ status: "success" });
+
+  return new Response(JSON.stringify({ status: "success" }), { status: 200 });
 }
+
