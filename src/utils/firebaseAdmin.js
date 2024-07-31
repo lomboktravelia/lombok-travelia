0bfe0dd2c9e5479fd847bb9c97e872999d83d885
OmMahen <102027819+OmMahen@users.noreply.github.com>
2024-07-31 10:16:05 +0700

    Update firebaseAdmin.js

--- src/utils/firebaseAdmin.js
+++ src/utils/firebaseAdmin.js
@@ -1,10 +1,10 @@
 var admin = require("firebase-admin");
-
+const { privateKey } = JSON.parse(process.env.PRIVATE_KEY);
 const serviceAccount = {
   type: process.env.TYPE,
   project_id: process.env.PROJECT_ID,
   private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY,
+  private_key: privateKey,
   client_email: process.env.CLIENT_EMAIL,
   client_id: process.env.CLIENT_ID,
   auth_uri: process.env.AUTH_URI,
